bd585e37ed1f8fb8982e5b87a33f5adf7c1d73f7
n_takahashi <jin.noc.sub@icloud.com>
2018-10-21 11:57:43 +0900

    init

---
+++ firebase/functions/index.js
@@ -0,0 +1,202 @@
+'use strict';
+const { dialogflow } = require('actions-on-google');
+const app = dialogflow({ debug: true });
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+var fs = require('fs');
+
+var f = require('./secret.js');
+
+admin.initializeApp({
+  credential: admin.credential.cert(f.secret())
+});
+
+var db = admin.firestore()
+
+const firestore = admin.firestore()
+const settings = {/* your settings... */ timestampsInSnapshots: true};
+firestore.settings(settings);
+// admin.initializeApp(functions.config().firebase);
+// const agent = new WebhookClient({ request, response });
+const moment = require('moment-timezone');
+
+async function isConversatinSet(UserIdFromGoogleHome){
+  let userRef = await db.collection("users")
+  let userDoc = await userRef.doc(UserIdFromGoogleHome).get().then(s => s.data())
+
+  if(userDoc===undefined){
+    // 初めて起動するユーザー
+    await createUser(UserIdFromGoogleHome)
+    return false
+  }else if(userDoc["conversation"]!== undefined || userDoc["conversation"]!== null){
+    // 会話がセットされているユーザー
+    return true
+  }else{
+    // 会話がセットされていないユーザー
+    return false
+  }
+}
+
+async function createUser(UserIdFromGoogleHome){
+  let userRef = await db.collection("users")
+  let userDocRef = await userRef.doc(UserIdFromGoogleHome)
+  await userDocRef.set({
+    name: "デフォルトさん",
+    conversation:null
+  })
+  return true
+}
+
+async function createConversation(conversationJson){
+  let convRef = await db.collection("conversations")
+  let docRef = await convRef.add(conversationJson)
+  let createdConvId = await docRef.id
+  return createdConvId
+}
+
+async function setConvOnUser(userId,convId){
+  let userRef = await db.collection("users")
+  let userDocRef = await userRef.doc(userId)
+  await userDocRef.update({
+    conversation:convId
+  })
+  // here!
+}
+
+app.intent('Default Welcome Intent', async conv => {
+
+  // ユーザーがdbになかったら登録もする
+  let convAlreadySetBool = await isConversatinSet(conv.user.id)
+
+  if(convAlreadySetBool){
+
+    let config = {
+      "mode": "prod",
+      "sectenceFieldToConfirm": "",
+      "welcome_prompt": "",
+      "conversation": {
+        "1": "会話２",
+        "2": "会話３",
+        "length": "2",
+        "welcome": "ほげ",
+        "user": `ABwppHF68tHYihEa8HHucf52nkcb4XA4WnFMgbO46cXQ782bH03KSOlR5zBQmKShSwVEFoxSOwKkQfk0hbRZYYuFJsxNrg`
+      }
+    }
+    let convRef = await db.collection("conversations")
+    let docRef = await convRef.add(
+      config["conversation"]
+    )
+    let createdConvId = await docRef.id
+
+    console.log(createdConvId);
+
+
+    conv.data.config = config
+    conv.data.config.counter = 0
+    let speach = config["conversation"]["welcome"]
+    conv.contexts.set('prodSession', Number(config["conversation"]["length"]), {});
+
+    if(config["conversation"]["length"] === 0){
+      conv.close("<speak><prosody rate='slow'>" + speach + "</prosody></speak>");
+    }else{
+      conv.ask("<speak><prosody rate='slow'>" + speach + "</prosody></speak>");
+    }
+
+
+  }else{
+
+    let speach = "このアプリでは、あなたが思った通りに会話をデザインできます。では、会話の設定を始めます。アプリの起動時に、私はなんと言えばいいですか？"
+    conv.data.config = {
+      "mode":"setting",
+      "sectenceFieldToConfirm":"",
+      "conversation":{
+        "length":"0",
+        "welcome":"",
+        "1":""
+      }
+    }
+    conv.contexts.set('welcomePromptConfig', 1, {});
+
+    conv.ask(speach);
+
+  }
+});
+
+
+app.intent('welcomePromptConfig-Intent', async conv => {
+  let welcomePromptToConfirm = conv.input.raw
+  conv.data.config.conversation.welcome = welcomePromptToConfirm
+  let speach = `起動時の私の発話は、${welcomePromptToConfirm}で良いですか？`
+  conv.contexts.set('welcomePromptConfirmation', 1, {});
+  conv.ask(speach);
+});
+
+app.intent('welcomePromptConfirmation-yesIntent', async conv => {
+  let speach = `起動時の発話を設定しました。まだ会話を続けますか？`
+  conv.contexts.set('confirmSessionContinue', 1, {});
+  conv.ask(speach);
+});
+
+app.intent('welcomePromptConfirmation-noIntent', async conv => {
+  let speach = `すみません。もう一度教えてください。アプリの起動時に、私はなんと言えばいいですか？`
+  conv.contexts.set('welcomePromptConfig', 1, {});
+  conv.ask(speach);
+});
+
+app.intent('confirmSessionContinue-yesIntent', async conv => {
+  let speach = `では次の会話で私はなんと言えば良いですか？`
+  conv.contexts.set('sessionSpeachConfig', 1, {});
+  conv.ask(speach);
+});
+
+app.intent('confirmSessionContinue-noIntent', async conv => {
+  let coversationToCreate = conv.data.config
+  coversationToCreate["conversation"]["user"] = conv.user.id
+  let convId = await createConversation(coversationToCreate["conversation"])
+  await setConvOnUser(conv.user.id,convId)
+
+  let speach = `会話の設定が完了しました。次回起動時に仰せの通りにお話しします。`
+  conv.close(speach);
+});
+
+app.intent('sessionSpeachConfig-Intent', async conv => {
+  let sessionSpeachToConfirm = conv.input.raw
+  let speach = `私は${sessionSpeachToConfirm}と言えば良いですか？`
+  conv.data.config.sectenceFieldToConfirm = sessionSpeachToConfirm
+  conv.contexts.set('sessionSpeachConfirmation', 1, {"text":sessionSpeachToConfirm});
+  conv.ask(speach);
+});
+
+app.intent('sessionSpeachConfirmation-yesIntent', async conv => {
+  let nowNum = Number(conv.data.config.conversation["length"]) + 1
+  conv.data.config.conversation["length"] = String(nowNum)
+  conv.data.config.conversation[String(nowNum)] = conv.data.config.sectenceFieldToConfirm
+  conv.data.config.sectenceFieldToConfirm = ""
+  let speach = `設定しました。${String(conv.data.config.conversation["length"])}回分の発話を設定しています。まだ会話を続けますか？`
+  conv.contexts.set('confirmSessionContinue', 1, {});
+  conv.ask(speach);
+});
+
+app.intent('sessionSpeachConfirmation-noIntent', async conv => {
+  let speach = `すみません。もう一度教えてください。次の会話では、私はなんと言えばいいですか？`
+  conv.contexts.set('sessionSpeachConfig', 1, {});
+  conv.ask(speach);
+});
+
+
+app.intent('prodSession-Intent', async conv => {
+  conv.data.config.counter += 1
+  let speach = conv.data.config.conversation[String(conv.data.config.counter)]
+  console.log(JSON.stringify(conv.data.config))
+  if(String(conv.data.config.counter) === String(conv.data.config.conversation["length"])){
+    conv.close(speach);
+  }else{
+    // conv.contexts.set('prodSession', 1, {});
+    conv.ask(speach)
+  }
+
+});
+
+
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
---
+++ firebase/functions/secret.js
@@ -0,0 +1,14 @@
+exports.secret = function() {
+        return {
+          "type": "service_account",
+          "project_id": "customize-conversation",
+          "private_key_id": "0caddba94229c951e1c1d10b12362eb244b3594a",
+          "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC0hqK5BjnEnEOT\nqehEYI9R1Apb2xyPGHD2adO9OHbfP6n0aNvC9E/Ba/FdktiQWQEl0Djwh5qfrF7D\nETl3VomMvbQPacKSAT/cFsOcOXV4ADPiDROdkxE+HR3O7opFwxOs7lLIboVc+fl0\npXF+jDsVtWkoIlp6vPp87BgK+wY2yXBEm+pqps7cOolV9kJ03s3LLJTGxgFW5tvV\nJtEaCG1zZtuymJwdY+onCwRMp2Bv52xMyVcgnJQW12rrcJHoXAcpgrQDILPtwvrP\nptFQoeIWQhvp7KhMQTBd0np9MehkSDKHMI8YTqNC4KNz2BkybdlFfHk4Zl3+AsfB\nlN8S8o1zAgMBAAECggEAM3Qyeiknmv3tFu/1J1QcVXbp8u97bt/JBrr4InUvEIet\nUd/3T1RiTR+OnpylxFLSDGySZiBIdp1jLxdAOV79epNxeHks/3+Cq6EDZQpNjIQG\nRbZzclTCM+GJpvr/h03IVGBjUQSLV4paSyCCvk6K4q+fxtQv1M77WFLUsYCF72Q1\nYnZPzDE9QwKJZCvvyCBuRq9lkjRpMdwJ05zLUS6h+W0mUXFmWcBZb5ipEcf/kKst\nNuWeagRl0RSQvHaRgalrM3vbix250dbvt44AUYUcuFUl1a1R0V5kGfyDmJI0xJK8\nFVvNTXtouK6IaxKdVr2+M2K1XuqxzvzCk5Vecxi+bQKBgQDYJPmh+QWGhNqdYTIL\nPXG7k6YICXLh190CjFG7/REPcjdw8nXzj7Qdrr207J0KN23UKoQjsN0S5JJt+oYK\nyfUWKGWPZcg1xP3vBDHRdC4LoopzvdVLJai8awPdYwBxHUSznUsXlQgF5FAwpk/6\nVaWmfSLr9aq+j8fdK0LYZwd87QKBgQDV0Ez1tSH/0ZAryJ5dDgVQHsPv65XxRdvM\nQYaoSHycUB+gKfmdfBZA8sRv89GplcK7XHhl5V2j+QagZ8t3VZ66nG/mQ8RZ2Oox\nPI9uV7VIMH8yi7jAnjqxBp9re5Gpnk6HGd1tPnQ3AatQwi8DQ0eSeZ3ac5xDUVUe\ngp0SzQNH3wKBgCLCF27Hp+7Q8IL8fVq3X9HFVZ7XPiU5XG4reH1dnd+5A1djdjpb\nsqBPWGx+3GYnMb87sxI2P+bo4O9UqTeU8JfGmUZtHLwg2KEWMJi5MSpn0hH4xhMW\nlELwhqeUYx7NkR29PrUlHywrYgXgyN295iyW1QXS9Mk+P+q+KTq5nselAoGBAI/I\nzHhPntWR8AoBl72sOBZxGgK1wtfFytHJHuz8EhBsMVhb4ejuEWkL2N4QcOZCN/oX\nbT/YxugGB1bHdiSCB4Qmv3lyCRECtjr2Jt5lTRuGwYMUQuIFhiKV+Y8mQgZ21wgz\ndJyKa0yyaqqca1EeZXszHdxR8ruybjNeUpLaG2KxAoGBAL5mgsOziH+xBPmDGWKC\nhxbw3ZlRwdh4sdgr1WTxIvbBsRGCGVsdlirrsPaUnRfsXQl5nJjFW1mQyEzle04I\nIAa8Plf+DWHl+E2I446GiMp1xRqs0XC1RqErDe/or9zyMJwFfqRATT5kc1TOUzSc\nil5GApFeEAzESdJtTsOd6ALB\n-----END PRIVATE KEY-----\n",
+          "client_email": "firebase-adminsdk-x5p4i@customize-conversation.iam.gserviceaccount.com",
+          "client_id": "116937346461011837598",
+          "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+          "token_uri": "https://oauth2.googleapis.com/token",
+          "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+          "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-x5p4i%40customize-conversation.iam.gserviceaccount.com"
+        };
+};
